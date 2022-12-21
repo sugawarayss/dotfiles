da1f8afc86a98885bea62eb03c32fe2601d19f7f
sugawarayss <sugawarayss@inap-vision.co.jp>
2022-12-21 10:23:41 +0900

    feat: update keymap.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -36,9 +36,6 @@ keymap("n", "gl", "gt", opts)
 keymap("n", "ss", ":split<Return><C-w>w", opts)
 keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
 
--- Do not yank with x
-keymap("n", "x", '"_x', opts)
-
 -- Delete a word backwards
 keymap("n", "dw", 'vb"_d', opts)
 
@@ -59,8 +56,13 @@ keymap("n", "<Esc><Esc>", ":<C-u>set nohlsearch<Return>", opts)
 -- Press jk fast to exit insert mode
 keymap("i", "jk", "<ESC>", opts)
 
--- コンマの後に自動的にスペースを挿入
-keymap("i", ",", ",<Space>", opts)
+-- 検索結果を画面中央に持ってくる
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz",  opts)
+keymap("n", "*", "*zz", opts)
+keymap("n", "#", "#zz", opts)
+keymap("n", "g*", "g*zz", opts)
+keymap("n", "g#", "g#zz", opts)
 
 -- Visual --
 -- Stay in indent mode
