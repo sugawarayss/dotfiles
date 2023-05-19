a10de9b91edd8fa6e8061fd3b16bc250ed2a3a23
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-05-19 21:41:25 +0900

    style: reformat lua files and style: reindent

---
+++ HOME/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,64 @@
+vim.opt.cmdheight = 0
+local function myMiniView(pattern, kind)
+  kind = kind or ""
+  return {
+    view = "mini",
+    filter = {
+      event = "msg_show",
+      kind = kind,
+      find = pattern,
+    },
+  }
+end
+require('noice').setup {
+  cmdline = {
+      format = {
+          cmdline = { pattern = '^:', icon = ' ', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+      },
+  },
+  popupmenu = {
+    backend = "nui",
+  },
+  lsp = {
+      override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+      },
+  },
+  views = {
+    cmdline_popup = {
+      position = {
+        row = "40%",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = "45%",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
+    },
+  },
+}
+
+vim.keymap.set("n", "<leader>nl", function() require("noice").cmd("last")  end)
+vim.keymap.set("n", "<leader>nh", "<cmd>Noice telescope<CR>")
