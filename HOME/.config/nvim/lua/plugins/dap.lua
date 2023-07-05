07ee291cf19ba187482fa1e0d2983457f360f5f1
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-07-06 04:01:13 +0900

    update: debugger configuration

--- HOME/.config/nvim/lua/plugins/dap.lua
+++ HOME/.config/nvim/lua/plugins/dap.lua
@@ -1,10 +1,20 @@
 local venv_python = ".venv/bin/python"
 require("dap-python").setup(venv_python)
 
+-- local succeeded, _ = pcall(require("dap-python").setup, venv_python)
+-- if not succeeded then
+--   print("failure setup dap-python. is 'debugpy' installed? run `pip install debugpy`")
+-- end
+
 vim.api.nvim_set_keymap('n', '<F8>', ':DapContinue<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F9>', ':DapStepOver<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F10>', ':DapStepInto<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<F7>', ':DapStepOut<CR>', { silent = true })
+
+vim.api.nvim_set_keymap('n', '<Leader>tm', ":lua require('dap-python').test_method()<CR>", { silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>tc', ":lua require('dap-python').test_class()<CR>", { silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>ts', "<ESC>:lua require('dap-python').debug_selection()<CR>", { silent = true })
+
 vim.api.nvim_set_keymap('n', '<leader>b', ':DapToggleBreakpoint<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>B', ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Breakpoint condition: "))<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>lp', ':lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', { silent = true })
