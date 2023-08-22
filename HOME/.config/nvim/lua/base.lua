fd36616ef30bafe44a1ca51e6e873483c2d9b2c3
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-08-22 11:58:55 +0900

    update: base nvim configuration remove memo function

--- HOME/.config/nvim/lua/base.lua
+++ HOME/.config/nvim/lua/base.lua
@@ -5,6 +5,6 @@ vim.scriptencoding = "utf-8"
 vim.wo.number = true
 
 -- Open hoge file
-vim.api.nvim_create_user_command("Memo", function(opts)
-  vim.cmd("e " .. "~/.config/nvim/memo/memo.markdown")
-end, {})
+-- vim.api.nvim_create_user_command("Memo", function(opts)
+--   vim.cmd("e " .. "~/.config/nvim/memo/memo.markdown")
+-- end, {})
