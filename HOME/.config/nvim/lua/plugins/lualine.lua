f221b49d3511daf59c1c7398eb7a6f3f87501e84
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-10-14 10:23:19 +0900

    fix: healthcheck alert

--- HOME/.config/nvim/lua/plugins/lualine.lua
+++ HOME/.config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
-    { "nvim-tree/nvim-web-devicons", opt = true },
+    { "nvim-tree/nvim-web-devicons"},
   },
   config = function()
     require("lualine").setup({
