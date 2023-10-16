8d23a13fd8e2eed503585b078799e5f81d7f7e51
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-10-16 21:53:50 +0900

    update: nvim-colorizer lazy load at BufWinEnter event

--- HOME/.config/nvim/lua/plugins/colorizer.lua
+++ HOME/.config/nvim/lua/plugins/colorizer.lua
@@ -1,6 +1,10 @@
 -- カラーコードを色付け
 return {
   "norcalli/nvim-colorizer.lua",
+  -- 遅延読み込みする
+  lazy = true,
+  -- バッファがウィンドウに表示された時にロードする
+  event = { "BufWinEnter" },
   config = function()
     require("colorizer").setup({
       "*", -- Highligt all files
