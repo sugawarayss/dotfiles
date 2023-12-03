cc547e288ae0c66cf02c3d5c8a406bbc37b1bbf5
sugawarayss <sugawarayss@inap-vision.co.jp>
2023-12-03 16:28:52 +0900

    change: keymap of show File Explorer

--- HOME/.config/nvim/lua/plugins/neo-tree.lua
+++ HOME/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,9 +1,10 @@
+-- File Explorer プラグイン
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   lazy = true,
   keys = {
-    { "<C-n>", ":Neotree toggle<CR>", mode = "n", desc = "Toggle File Exploer" },
+    { "<C-t><C-t>", ":Neotree toggle<CR>", mode = "n", desc = "Toggle File Exploer" },
     { "<M-g><M-s>", ":Neotree float git_status<CR>", mode = "n", desc = "Show Git Status by Neotree"},
   },
   depencencies = {
