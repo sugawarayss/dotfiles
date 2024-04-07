05a9800f45c5622b9c3ccc95f4706379460c4985
sugawarayss <sugawarayss@inap-vision.co.jp>
2024-04-08 02:40:01 +0900

    update: prettier config of null-ls

--- HOME/.config/nvim/lua/plugins/null-ls.lua
+++ HOME/.config/nvim/lua/plugins/null-ls.lua
@@ -102,6 +102,7 @@ return {
       -- JavaScript/TypeScript
       null_ls.builtins.formatting.prettier.with({ -- FIXME: 効かない。。。
         filetypes = { "javascript", "typescript", "jsx", "html", "css", "scss", "markdown" },
+        only_local = "node_modules/.bin",
       }),
       null_ls.builtins.diagnostics.eslint_d.with({
         filetypes = { "javascript", "typescript", "jsx", "html", "css", "scss", "markdown" },
