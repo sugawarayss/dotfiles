4f3fde9e98445b969346897c6b2459314995a8ed
sugawarayss <sugawarayss@inap-vision.co.jp>
2024-09-16 12:46:59 +0900

    update: markdown plugin configure

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -18,11 +18,33 @@ return {
     "MeanderingProgrammer/markdown.nvim",
     lazy = true,
     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     ft = { "markdown" },
     build = ":RenderMarkdown toggle" ,
     config = function()
-      require("render-markdown").setup()
+      require("render-markdown").setup({
+        file_types = { "markdown", "vimwiki" },
+        -- 見出しの表示設定
+        heading = { border = true },
+        -- code blockの表示設定
+        code = {
+          sign = false,
+          position = 'right',
+          width = 'block',
+          min_width = 45,
+          left_pad = 2,
+          language_pad = 2,
+        },
+        -- tableの表示設定
+        pipe_table = { 
+          preset = "heavy",
+          alignment_indicator = "┅",
+        },
+        -- call outの表示設定
+        callout = { note = { rendered = '󰅾 Notary' } },
+        -- インデントの表示設定
+        indent = { enabled = true, per_level = 2 },
+      })
     end,
   }
 }
