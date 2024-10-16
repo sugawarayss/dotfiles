2ba6afc5a447fd20bad2cebc3e5d3fa3066e533f
sugawarayss <sugawarayss@inap-vision.co.jp>
2024-10-16 11:29:38 +0900

    update: dap configure

--- HOME/.config/nvim/lua/plugins/dap.lua
+++ HOME/.config/nvim/lua/plugins/dap.lua
@@ -48,14 +48,44 @@ return {
         }
       }
     })
-    require('dap-python').setup('./.venv/bin/python')
+    local debugpy_path = require("mason-registry").get_package("debugpy"):get_install_path()
+    require('dap-python').setup(debugpy_path .. "/venv/bin/python")
+    require("dap-python").test_runner = "pytest"
+
+    -- require("dap").adapters.python = function(cb, config)
+    --   if config.request == 'attach' then
+    --     ---@diagnostic disable-next-line: undefined-field
+    --     local port = (config.connect or config).port
+    --     ---@diagnostic disable-next-line: undefined-field
+    --     local host = (config.connect or config).host or '127.0.0.1'
+    --     cb({
+    --       type = 'server',
+    --       port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+    --       host = host,
+    --       options = {
+    --         source_filetype = 'python',
+    --       },
+    --     })
+    --   else
+    --     cb({
+    --       type = 'executable',
+    --       command = debugpy_path .. '/venv/bin/python',
+    --       args = { '-m', 'debugpy.adapter' },
+    --       options = {
+    --         source_filetype = 'python',
+    --       },
+    --     })
+    --   end
+    -- end
     table.insert(require('dap').configurations.python, {
       type = 'python',
       request = 'launch',
       name = "Launch file",
       program = "${file}",
       pythonPath = function()
-        return vim.fn.input('Path to python interpreter: ', vim.fn.getcwd() .. '/.venv/bin/python')
+        print(vim.fn.getcwd() .. '/.venv/bin/python')
+        local cwd = vim.fn.getcwd()
+        return vim.fn.input('Path to python interpreter: ', cwd .. '/.venv/bin/python')
       end,
     })
   end,
