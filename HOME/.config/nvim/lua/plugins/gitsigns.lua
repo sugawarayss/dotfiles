0384934f5b56b7b1dc651d209675aa0c285a76dc
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-03-06 10:03:39 +0900

    style: gitsigns.lua

--- HOME/.config/nvim/lua/plugins/gitsigns.lua
+++ HOME/.config/nvim/lua/plugins/gitsigns.lua
@@ -19,18 +19,18 @@ return {
       linehl                       = false, --  バッファ本文のハイライトを行単位で変更する `Gitsigns toggle_linehl`
       word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir                 = {
-        interval = 1000,
-        follow_files = true
+        interval                   = 1000,
+        follow_files               = true
       },
-      auto_attach = true,
+      auto_attach                  = true,
       attach_to_untracked          = true,
       current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts      = {
-        virt_text          = true,
-        virt_text_pos      = "right_align", -- "eol" | "overlay" | "right_align"
-        delay              = 1000,
-        ignore_whitespace  = false,
-        virt_text_priority = 100,
+        virt_text                  = true,
+        virt_text_pos              = "right_align", -- "eol" | "overlay" | "right_align"
+        delay                      = 1000,
+        ignore_whitespace          = false,
+        virt_text_priority         = 100,
       },
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
       sign_priority                = 6,
@@ -39,11 +39,11 @@ return {
       max_file_length              = 40000, -- Disable if file is longer than this (in lines)
       preview_config               = {
         -- Options passed to nvim_open_win
-        border   = "single",
-        style    = "minimal",
-        relative = "editor",
-        row      = 0,
-        col      = 1,
+        border                     = "single",
+        style                      = "minimal",
+        relative                   = "editor",
+        row                        = 0,
+        col                        = 1,
       },
       on_attach = function(bufnr)
         local gs = require("gitsigns")
