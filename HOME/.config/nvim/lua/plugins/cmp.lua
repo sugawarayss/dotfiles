c5f2ea640a882108e0d51191717262323b932be9
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-05-12 11:10:26 +0900

    update: split nvim-autopairs.nvim config file

--- HOME/.config/nvim/lua/plugins/cmp.lua
+++ HOME/.config/nvim/lua/plugins/cmp.lua
@@ -146,31 +146,6 @@ return {
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end
   },
-  -- 括弧を自動で補完する
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({
-        -- Default Config
-        disable_filetype                  = {"TelescopePrompt", "spectre_panel", "snacks_picker_input"},
-        disable_in_macro                  = true,
-        disable_in_visualblock            = false,
-        disable_in_replace_mode           = true,
-        ignored_next_char                 = [=[[%w%%%'%[%"%.%`%$]]=],
-        enable_moveright                  = true,
-        enable_afterquote                 = true,
-        enable_check_bracket_line         = true,
-        enable_moveright_bracket_in_quote = true,
-        enable_abbr                       = false,
-        break_undo                        = true,
-        check_ts                          = false,
-        map_cr                            = true,
-        map_bs                            = true,
-        map_c_h                           = false,
-        map_c_w                           = false,
-      })
-    end
-  },
   -- Github Copilotのサジェストを補完リストに表示する
   {
     "zbirenbaum/copilot-cmp",
