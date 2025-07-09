b47c3e4681df1eb35c0e7ce9d44e82484f208f29
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-07-09 11:15:17 +0900

    fix: disable modes.nvim highlight on visual mode

--- HOME/.config/nvim/lua/plugins/modes.lua
+++ HOME/.config/nvim/lua/plugins/modes.lua
@@ -10,7 +10,7 @@ return {
         copy = color_palette.cyan,
         delete = color_palette.red1,
         insert = color_palette.green2,
-        visual = color_palette.magenta,
+        -- visual = color_palette.magenta,
       },
       -- Set opacity for cursorline and number background
       line_opacity = 0.4,
