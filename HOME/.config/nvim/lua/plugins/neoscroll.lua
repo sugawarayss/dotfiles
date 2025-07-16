633228c20934717303282e8dd397e822f26264f2
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-07-16 15:04:56 +0900

    update: neoscroll easing type

--- HOME/.config/nvim/lua/plugins/neoscroll.lua
+++ HOME/.config/nvim/lua/plugins/neoscroll.lua
@@ -20,7 +20,7 @@ return {
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
       duration_multiplier = 1.0, -- Global duration multiplier
-      easing = "linear", -- Default easing function
+      easing = "circular", -- Default easing function. linear | quadratic | cubic | quartic | quintic | circular | sine
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
       performance_mode = false, -- Disable "Performance Mode" on all buffers.
