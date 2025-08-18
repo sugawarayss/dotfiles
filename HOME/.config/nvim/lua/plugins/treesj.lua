bf76bd498cf38f56bef5e20e9039e042baaea95b
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-08-18 11:06:57 +0900

    update: treesj add keymap by which-key

--- HOME/.config/nvim/lua/plugins/treesj.lua
+++ HOME/.config/nvim/lua/plugins/treesj.lua
@@ -1,18 +1,10 @@
 -- コードを分割したり結合したりできるようにするプラグイン
 return {
   "Wansmer/treesj",
-  keys = {
-    {
-      "<leader>m",
-      function()
-        require("treesj").toggle()
-      end,
-      mode = "n",
-      desc = "配列やオブジェクトを分割/結合する",
-    },
-  },
+  event = "VeryLazy",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
+    local wk = require("which-key")
     require("treesj").setup({
       use_default_keymaps = true,
       check_syntax_error = true,
@@ -21,6 +13,9 @@ return {
       notify = true,
       dot_repeat = true,
       on_error = nil,
+    })
+    wk.add({
+      { "<leader>m", "<cmd>TSJToggle<CR>", mode = "n", icon = "󰙅", desc = "配列やオブジェクトを分割/結合する" },
     })
   end,
 }
