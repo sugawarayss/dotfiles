300448c51e8e6a4c5383690e0199d90b731d5f40
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-09-20 11:34:34 +0900

    update: override Highlight Group color

--- HOME/.config/nvim/lua/plugins/colortheme.lua
+++ HOME/.config/nvim/lua/plugins/colortheme.lua
@@ -7,6 +7,32 @@ return {
     require("onedark").setup({
       -- Default theme style. ["dark", "darker", "cool", "deep", "warm", "warmer", "light"]
       style = "warmer",
+      -- black = "#101012",
+      -- bg0 = "#232326",
+      -- bg1 = "#2c2d31",
+      -- bg2 = "#35363b",
+      -- bg3 = "#37383d",
+      -- bg_d = "#1b1c1e",
+      -- bg_blue = "#68aee8",
+      -- bg_yellow = "#e2c792",
+      -- fg = "#a7aab0",
+      -- purple = "#bb70d2",
+      -- green = "#8fb573",
+      -- orange = "#c49060",
+      -- blue = "#57a5e5",
+      -- yellow = "#dbb671",
+      -- cyan = "#51a8b3",
+      -- red = "#de5d68",
+      -- grey = "#5a5b5e",
+      -- light_grey = "#818387",
+      -- dark_cyan = "#2b5d63",
+      -- dark_red = "#833b3b",
+      -- dark_yellow = "#7c5c20",
+      -- dark_purple = "#79428a",
+      -- diff_add = "#282b26",
+      -- diff_delete = "#2a2626",
+      -- diff_change = "#1a2a37",
+      -- diff_text = "#2c485f",
       -- Show/hide background
       transparent = true,
       -- Change terminal color as per the selected theme style
@@ -18,7 +44,7 @@ return {
 
       -- Toggle theme style --
       -- keybind to toggle theme style, Leave it nil to disable it
-      toggle_style_key = "<Leader>ts",
+      toggle_style_key = nil,
       toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
       -- Change code style --
       -- Options are itaclic, bold, unserline, none
@@ -41,7 +67,16 @@ return {
       -- Override default colors
       colors = {},
       -- Override highlight groups
-      highlights = {},
+      highlights = {
+        -- 対応する括弧のハイライト
+        MatchParen = { bg = "#37383d" },
+        -- Flashによるジャンプや置換対象のハイライト
+        Substitute = { fg = "#232326", bg = "#8fb573", fmt = "bold" },
+        -- hop.nvimのラベル色
+        HopNextKey = { fg = "#de5d68", fmt = "bold" },
+        HopNextKey1 = { fg = "#57a5e5", fmt = "bold" },
+        HopNextKey2 = { fg = "#51a8b3", fmt = "bold" },
+      },
 
       -- Plugins Config --
       diagnostics = {
