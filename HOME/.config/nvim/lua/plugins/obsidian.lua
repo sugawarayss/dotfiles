789ff8536f86b0ae2b94600dff79bea037a628c8
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-09-20 11:37:45 +0900

    replace: obsidian.nvim with configure

--- HOME/.config/nvim/lua/plugins/obsidian.lua
+++ HOME/.config/nvim/lua/plugins/obsidian.lua
@@ -1,61 +1,39 @@
 -- vim から Obsidian を操作するプラグイン
 vim.opt.conceallevel = 1
-vim.cmd([[cab tag ObsidianTags]])
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*",
   lazy = true,
   ft = "markdown",
-  cmd = {
-    "ObsidianOpen", -- Obsidianアプリでノートを開く
-    -- "ObsidianNew", -- 新しいノートを作成する
-    -- "ObsidianQuickSwitch", -- pickerでノートの名前で検索してバッファとして開く
-    -- "ObsidianFollowLink", -- カーソル位置のノートへのリンクを画面分割で開く
-    -- "ObsidianBacklink", -- 現在のバッファへの参照をpicker で開く
-    -- "ObsidianTags", -- 入力したタグ名を含むノートをpickerで開く
-    "ObsidianToday", -- 今日分のデイリーノートを開く
-    -- "ObsidianTomorrow", -- 次の平日のデイリーノートを開く(週末を考慮する)
-    -- "ObsidianDailies", -- デイリーノート一覧をpickerで開く
-    "ObsidianTemplate", -- テンプレートを選択してノートを作成する
-    -- "ObsidianSearch", -- pickerを使ってノートを検索 (または作成) する
-    -- "ObsidianLink", -- 選択したテキストをノートへのリンクに変換する
-    -- "ObsidianLinkNew", -- 新しいノートを作成して、選択したテキストがあれば、そのノートへのリンクに変換する
-    -- "ObsidianLinks", -- 現在のバッファにあるノートへのリンクをpickerで開く
-    -- "ObsidianExtractNote", -- 選択したテキストをノートに抽出して、それにリンクする
-    -- "ObsidianWorkspace", -- 別のワークスペースに切り替える
-    -- "ObsidianPasteImg", -- クリップボードから画像を保存して、Markdown画像リンクとして追加する
-    -- "ObsidianRename", -- カーソル位置のノート名を変更する(全体のリンクも変更される)
-    "ObsidianToggleCheckbox", -- チェックボックスをトグルする
-    "ObsidianNewFromTemplate", -- テンプレートフォルダ内のテンプレートから新しいノートを作成する
-    "ObsidianTOC", -- 現在のノートの目次をpicker list に読み込む
-  },
+  cmd = { "Obsidian" },
   keys = {
     {
-      "<Leader>sn",
-      "<Cmd>ObsidianQuickSwitch<CR>",
+      ";son",
+      "<Cmd>Obsidian quick_switch<CR>",
       mode = "n",
       { noremap = true, silent = true },
       desc = "Obsidianノートを検索",
     },
     {
       "<Leader>nn",
-      "<Cmd>ObsidianNewFromTemplate<CR>",
+      "<Cmd>Obsidian new_from_template<CR>",
       mode = "n",
       { noremap = true, silent = true },
       desc = "Obsidianノートを開く(作成)",
     },
     {
       "<Leader>dn",
-      "<Cmd>ObsidianToday<CR>",
+      "<Cmd>Obsidian today<CR>",
       mode = "n",
       { noremap = true, silent = true },
       desc = "Obsidianデイリーノートを開く(作成)",
     },
   },
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    { "nvim-lua/plenary.nvim", lazy = true },
   },
   opts = {
+    legacy_commands = false,
     open_notes_in = "vsplit",
     workspaces = {
       {
@@ -75,8 +53,12 @@ return {
       end
       return out
     end,
+    picker = {
+      name = "snacks.pick",
+    },
     daily_notes = {
       folder = "daily",
+      date_format = "%Y-%m-%d",
       template = "daily.md",
     },
     completion = {
@@ -86,7 +68,7 @@ return {
       min_chars = 2,
     },
     templates = {
-      subdir = "templates",
+      folder = "templates",
       date_format = "%Y-%m-%d",
       time_format = "%H:%M",
     },
