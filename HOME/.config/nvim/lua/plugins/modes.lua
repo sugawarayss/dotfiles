93e3aefd2a1a47e4a143fb26156ce21e73199e7a
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-09-22 12:37:20 +0900

    update: modes color

--- HOME/.config/nvim/lua/plugins/modes.lua
+++ HOME/.config/nvim/lua/plugins/modes.lua
@@ -12,13 +12,13 @@ return {
         delete = color_palette.red,
         -- change = color_palette.orange,
         -- format = color_palette.cyan,
-        insert = color_palette.blue,
+        insert = color_palette.diff_text,
         replace = color_palette.purple,
         -- select = color_palette.dark_purple,
-        visual = color_palette.yellow,
+        visual = color_palette.orange,
       },
       -- Set opacity for cursorline and number background
-      line_opacity = 0.2,
+      line_opacity = 0.3,
       -- Enable cursor highlights
       set_cursor = true,
       -- Enable cursorline initially, and disable cursorline for inactive windows
