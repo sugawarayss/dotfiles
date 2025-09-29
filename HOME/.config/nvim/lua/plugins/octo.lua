0dadea85a437f66f95858217159a78426f904074
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-09-29 11:15:24 +0900

    update: octo color

--- HOME/.config/nvim/lua/plugins/octo.lua
+++ HOME/.config/nvim/lua/plugins/octo.lua
@@ -13,10 +13,25 @@ if not vim.g.vscode then
         })
       end,
       config = function()
+        local palette = require("onedark.colors")
         require("octo").setup({
           picker = "snacks", -- "telescope" or "fzf-lua"
           suppress_missing_scope = {
             projects_v2 = true,
+          },
+          colors = {
+            white = palette.fg,
+            grey = palette.grey,
+            black = palette.black,
+            red = palette.red,
+            dark_red = palette.dark_red,
+            green = palette.green,
+            dark_green = palette.dark_cyan,
+            yellow = palette.yellow,
+            dark_yellow = palette.dark_yellow,
+            blue = palette.blue,
+            dark_blue = palette.diff_text,
+            purple = palette.purple,
           },
         })
       end,
