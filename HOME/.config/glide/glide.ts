7cd600c6618cf9444e2e7e1502b732cdde509cbe
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-10-14 09:39:18 +0900

    update: glide config

--- HOME/.config/glide/glide.ts
+++ HOME/.config/glide/glide.ts
@@ -16,9 +16,38 @@
 //
 // Try typing `glide.` and see what you can do!
 
-glide.keymaps.set("normal", "<space>cr", "config_reload");
-// Shift + Command + c で URL コピー
-glide.keymaps.set("normal", "<S-D-c>", "url_yank");
-// Shift + h, Shift + l でタブ移動
-glide.keymaps.set("normal", "<S-h>", "tab_next");
-glide.keymaps.set("normal", "<S-l>", "tab_prev");
+// ヒントのフォントサイズを指定
+glide.o.hint_size = "16px";
+
+///////////////
+/// keymaps ///
+///////////////
+glide.keymaps.set("normal", "<leader>r", "reload", {
+  description: "画面をリロード",
+});
+glide.keymaps.set("normal", "<leader>R", "reload_hard", {
+  description: "スーパーリロード",
+});
+glide.keymaps.set("normal", "<leader>f", "hint --location=browser-ui", {
+  description: "ブラウザUIにヒントを表示",
+});
+glide.keymaps.set("normal", "f", "hint", { description: "画面内ヒントを表示" });
+glide.keymaps.set("normal", "<C-o>", "jumplist_back", {
+  description: "ジャンプリストを戻る",
+});
+glide.keymaps.set("normal", "<C-i>", "jumplist_forward", {
+  description: "ジャンプリストを進む",
+});
+
+glide.keymaps.set("normal", "yy", "url_yank", {
+  description: "現在のページURLをコピーする",
+});
+glide.keymaps.set("normal", "<S-D-c>", "url_yank", {
+  description: "現在のページURLをコピーする",
+});
+glide.keymaps.set("normal", "<S-h>", "tab_next", {
+  description: "次のタブを表示",
+});
+glide.keymaps.set("normal", "<S-l>", "tab_prev", {
+  description: "前のタブを表示",
+});
