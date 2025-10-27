5423eedba8883f5160b7e5eb2589eb12e57a6f43
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-10-27 09:42:08 +0900

    update: lasterisk.nvim

--- HOME/.config/nvim/lua/plugins/lasterisk.lua
+++ HOME/.config/nvim/lua/plugins/lasterisk.lua
@@ -3,29 +3,28 @@ return {
   {
     "rapan931/lasterisk.nvim",
     event = { "BufReadPre" },
-    keys = {
-      {
-        "*",
-        function()
-          require("lasterisk").search({ silent = true })
-          if not vim.g.vscode then
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        {
+          "*",
+          function()
+            require("lasterisk").search({ silent = true })
             require("hlslens").start()
-          end
-        end,
-        mode = "n",
-        desc = "カーソル位置の単語を検索してハイライト",
-      },
-      {
-        "g*",
-        function()
-          require("lasterisk").search({ is_whole = false, silent = true })
-          if not vim.g.vscode then
+          end,
+          mode = "n",
+          desc = "カーソル位置の単語を検索してハイライト",
+        },
+        {
+          "g*",
+          function()
+            require("lasterisk").search({ is_whole = false, silent = true })
             require("hlslens").start()
-          end
-        end,
-        mode = { "n", "x" },
-        desc = "単語区切りせずにカーソル位置の単語を検索してハイライト",
-      },
-    },
+          end,
+          mode = { "n", "x" },
+          desc = "単語区切りせずにカーソル位置の単語を検索してハイライト",
+        },
+      })
+    end,
   },
 }
