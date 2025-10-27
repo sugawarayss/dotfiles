936bbcc4c06de59ba3ab17b235c5b18fa16f21f8
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-10-27 09:42:47 +0900

    update: markdown plugins config

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -1,24 +1,26 @@
-if not vim.g.vscode then
-  return {
-    --markdownのSyntaxとキーマップを拡張するプラグイン
-    {
-      "ixru/nvim-markdown",
-      ft = { "markdown" },
-    },
-    -- markdownレンダリング
-    {
-      "OXY2DEV/markview.nvim",
-      ft = { "markdown", "codecompanion" },
-      opts = {
-        preview = {
-          filetypes = { "markdown", "codecompanion" },
-          ignore_buftypes = {},
-        },
+return {
+  --markdownのSyntaxとキーマップを拡張するプラグイン
+  {
+    "ixru/nvim-markdown",
+    ft = { "markdown" },
+    cond = function()
+      return not vim.g.vscode
+    end,
+  },
+  -- markdownレンダリング
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown", "codecompanion" },
+    cond = function()
+      return not vim.g.vscode
+    end,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {},
       },
     },
-    { "nvim-treesitter/nvim-treesitter", lazy = true },
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-  }
-else
-  return {}
-end
+  },
+  { "nvim-treesitter/nvim-treesitter", lazy = true },
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+}
