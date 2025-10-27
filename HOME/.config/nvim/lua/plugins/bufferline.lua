bbe7f357a98e174f6d1242b9d4c11507c993ff88
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-10-27 09:41:38 +0900

    update: bufferline.nvim

--- HOME/.config/nvim/lua/plugins/bufferline.lua
+++ HOME/.config/nvim/lua/plugins/bufferline.lua
@@ -1,122 +1,121 @@
-if not vim.g.vscode then
-  return {
-    -- タブごとに表示するバッファを切り替える
-    {
-      "akinsho/bufferline.nvim",
-      version = "*",
-      lazy = true,
-      event = { "BufReadPre" },
-      init = function()
-        local wk = require("which-key")
-        wk.add({
-          {
-            "L",
-            "<Cmd>BufferLineCycleNext<CR>",
-            mode = "n",
-            icon = "",
-            desc = "次のバッファを表示",
+return {
+  -- タブごとに表示するバッファを切り替える
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    lazy = true,
+    event = { "BufReadPre" },
+    cond = function()
+      return not vim.g.vscode
+    end,
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        {
+          "L",
+          "<Cmd>BufferLineCycleNext<CR>",
+          mode = "n",
+          icon = "",
+          desc = "次のバッファを表示",
+        },
+        {
+          "H",
+          "<Cmd>BufferLineCyclePrev<CR>",
+          mode = "n",
+          icon = "",
+          desc = "前のバッファを表示",
+        },
+      })
+    end,
+    config = function()
+      local color_palette = require("onedark.colors")
+      require("bufferline").setup({
+        highlights = {
+          error_selected = { fg = color_palette.red }, -- #e16d77
+          warning_diagnostic_selected = { fg = color_palette.orange }, -- #c99a6e
+          error_diagnostic_selected = { fg = color_palette.red }, -- #e16d77
+          close_button_selected = { fg = color_palette.yellow }, -- #dfbe81
+        },
+        options = {
+          themable = true,
+          numbers = function(opts)
+            return string.format("%s.%s", opts.ordinal, opts.lower())
+          end,
+          indicator = {
+            icon = "|  ",
+            style = "icon",
           },
-          {
-            "H",
-            "<Cmd>BufferLineCyclePrev<CR>",
-            mode = "n",
-            icon = "",
-            desc = "前のバッファを表示",
-          },
-        })
-      end,
-      config = function()
-        local color_palette = require("onedark.colors")
-        require("bufferline").setup({
-          highlights = {
-            error_selected = { fg = color_palette.red }, -- #e16d77
-            warning_diagnostic_selected = { fg = color_palette.orange }, -- #c99a6e
-            error_diagnostic_selected = { fg = color_palette.red }, -- #e16d77
-            close_button_selected = { fg = color_palette.yellow }, -- #dfbe81
-          },
-          options = {
-            themable = true,
-            numbers = function(opts)
-              return string.format("%s.%s", opts.ordinal, opts.lower())
-            end,
-            indicator = {
-              icon = "|  ",
-              style = "icon",
+          max_name_length = 100,
+          max_prefix_length = 15,
+          truncate_names = true,
+          tab_size = 30,
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, _, _, _)
+            return "(" .. count .. ")"
+          end,
+          -- NOTE: this will be called a lot so don't do any heavy processing here
+          custom_filter = function(buf_number)
+            -- filter out filetypes you don't want to see
+            if vim.bo[buf_number].filetype == "qf" then
+              return false
+            end
+            if vim.bo[buf_number].buftype == "terminal" then
+              return false
+            end
+            -- -- filter out by buffer name
+            if vim.api.nvim_buf_get_name(buf_number) == "" or vim.api.nvim_buf_get_name(buf_number) == "[No Name]" then
+              return false
+            end
+            if vim.api.nvim_buf_get_name(buf_number) == "[dap-repl]" then
+              return false
+            end
+            -- -- filter out based on arbitrary rules
+            -- -- e.g. filter out vim wiki buffer from tabline in your work repo
+            -- if vim.uv.cwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+            --   return true
+            -- end
+            return true
+          end,
+          offsets = {
+            {
+              filetype = "neo-tree",
+              text = "File Explorer",
+              text_align = "left",
+              separator = true,
             },
-            max_name_length = 100,
-            max_prefix_length = 15,
-            truncate_names = true,
-            tab_size = 30,
-            diagnostics = "nvim_lsp",
-            diagnostics_indicator = function(count, _, _, _)
-              return "(" .. count .. ")"
-            end,
-            -- NOTE: this will be called a lot so don't do any heavy processing here
-            custom_filter = function(buf_number)
-              -- filter out filetypes you don't want to see
-              if vim.bo[buf_number].filetype == "qf" then
-                return false
-              end
-              if vim.bo[buf_number].buftype == "terminal" then
-                return false
-              end
-              -- -- filter out by buffer name
-              if vim.api.nvim_buf_get_name(buf_number) == "" or vim.api.nvim_buf_get_name(buf_number) == "[No Name]" then
-                return false
-              end
-              if vim.api.nvim_buf_get_name(buf_number) == "[dap-repl]" then
-                return false
-              end
-              -- -- filter out based on arbitrary rules
-              -- -- e.g. filter out vim wiki buffer from tabline in your work repo
-              -- if vim.uv.cwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-              --   return true
-              -- end
+          },
+          color_icons = true,
+          get_element_icon = function(element)
+            local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
+            return icon, hl
+          end,
+          -- show_buffer_icons = true,
+          show_buffer_close_icons = true,
+          show_close_icon = true,
+          show_tab_indicators = true,
+          -- show_duplicate_prefix = true,
+          -- persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+          -- move_wraps_at_ends = false,
+          separator_style = "slant", -- slant | padded_slant | slope | padded_slope | thick | thin
+          enforce_regular_tabs = true,
+          always_show_bufferline = true,
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { "close" },
+          },
+          sort_by = function(buffer_a, buffer_b)
+            if not buffer_a and buffer_b then
               return true
-            end,
-            offsets = {
-              {
-                filetype = "neo-tree",
-                text = "File Explorer",
-                text_align = "left",
-                separator = true,
-              },
-            },
-            color_icons = true,
-            get_element_icon = function(element)
-              local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
-              return icon, hl
-            end,
-            -- show_buffer_icons = true,
-            show_buffer_close_icons = true,
-            show_close_icon = true,
-            show_tab_indicators = true,
-            -- show_duplicate_prefix = true,
-            -- persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-            -- move_wraps_at_ends = false,
-            separator_style = "slant", -- slant | padded_slant | slope | padded_slope | thick | thin
-            enforce_regular_tabs = true,
-            always_show_bufferline = true,
-            hover = {
-              enabled = true,
-              delay = 200,
-              reveal = { "close" },
-            },
-            sort_by = function(buffer_a, buffer_b)
-              if not buffer_a and buffer_b then
-                return true
-              elseif buffer_a and not buffer_b then
-                return false
-              end
-              return buffer_a.ordinal < buffer_b.ordinal
-            end,
-          },
-        })
-      end,
-    },
-    -- bufferline.nvim の依存プラグイン
-    { "nvim-tree/nvim-web-devicons", lazy = true },
-  }
-else
-  return {}
-end
+            elseif buffer_a and not buffer_b then
+              return false
+            end
+            return buffer_a.ordinal < buffer_b.ordinal
+          end,
+        },
+      })
+    end,
+  },
+  -- bufferline.nvim の依存プラグイン
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+}
