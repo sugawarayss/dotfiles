e5d74bcc42961ec01abc4f4935f1666a9ab28e96
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-11-07 18:36:34 +0900

    update: bufresize config

--- HOME/.config/nvim/lua/plugins/bufresize.lua
+++ HOME/.config/nvim/lua/plugins/bufresize.lua
@@ -4,7 +4,19 @@ return {
   lazy = true,
   event = { "VimEnter" },
   config = function()
+    local opts = { noremap = true, silent = true }
     require("bufresize").setup({
+      register = {
+        keys = {
+          -- バッファ幅を調整
+          -- { "n", "<M-h>", "5<C-w><", opts },
+          -- { "n", "<M-l>", "5<C-w>>", opts },
+          -- バッファ高を調整
+          { "n", "<M-y>", "5<C-w>+", opts },
+          { "n", "<M-u>", "5<C-w>-", opts },
+        },
+        trigger_events = { "BufWinEnter", "WinEnter" },
+      },
       resize = {
         keys = {},
         trigger_events = { "VimResized" },
