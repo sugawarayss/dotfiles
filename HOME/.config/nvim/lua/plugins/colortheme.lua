3f0f99d137c6d2905df4b46f97f6d09e48fd1265
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-11-19 14:40:53 +0900

    update: highlight override

--- HOME/.config/nvim/lua/plugins/colortheme.lua
+++ HOME/.config/nvim/lua/plugins/colortheme.lua
@@ -68,7 +68,9 @@ return {
       colors = {},
       -- Override highlight groups
       highlights = {
-        -- 対応する括弧のハイライト
+        -- カーソル位置の単語ハイライト
+        IlluminatedWordRead = { bg = "#2c485f" },
+        -- 対応する括弧ハイライト
         MatchParen = { bg = "#2c2d31" },
         -- Flashによるジャンプや置換対象のハイライト
         Substitute = { fg = "#232326", bg = "#8fb573", fmt = "bold" },
