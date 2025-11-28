9a617a13e35837c90cd6a86c707e2137e3c60e3a
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-11-28 09:54:07 +0900

    replace: scrollbar plugin; split: hlslens

---
+++ HOME/.config/nvim/lua/plugins/hlslens.lua
@@ -0,0 +1,45 @@
+-- 検索結果をvirtual textで表示するプラグイン
+return {
+  "kevinhwang91/nvim-hlslens",
+  enabled = true,
+  lazy = true,
+  event = { "BufReadPost" },
+  config = function()
+    require("hlslens").setup({
+      -- 検索結果に対してvirtual text (例[▼ 1/5] )を表示する設定
+      override_lens = function(render, posList, nearest, idx, relIdx)
+        local sfw = vim.v.searchforward == 1
+        local indicator, text, chunks
+        local absRelIdx = math.abs(relIdx)
+        if absRelIdx > 1 then
+          indicator = ("%d%s"):format(absRelIdx, sfw ~= (relIdx > 1) and "▲" or "▼")
+        elseif absRelIdx == 1 then
+          indicator = sfw ~= (relIdx == 1) and "▲" or "▼"
+        else
+          indicator = ""
+        end
+
+        -- lua 5.1と5.2+の互換のため
+        local unpck = unpack or table.unpack
+        local lnum, col = unpck(posList[idx])
+        if nearest then
+          local cnt = #posList
+          if indicator ~= "" then
+            text = ("[%s %d/%s]"):format(indicator, idx, cnt)
+          else
+            text = ("[%d/%d]"):format(idx, cnt)
+          end
+          chunks = { { " " }, { text, "HLSearchLensNear" } }
+        else
+          text = ("[%s %d]"):format(indicator, idx)
+          chunks = { { " " }, { text, "HLSearchLens" } }
+        end
+        render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+      end,
+      -- nvim-scrollbar に検索位置を表示する設定
+      build_position_cb = function(plist, _, _, _)
+        require("scrollbar.handlers.search").handler.show(plist.start_pos)
+      end,
+    })
+  end,
+}
--- HOME/.config/nvim/lua/plugins/scrollbar.lua
+++ HOME/.config/nvim/lua/plugins/scrollbar.lua
@@ -1,82 +1,35 @@
+-- スクロールバー拡張
 return {
-  -- スクロールバー拡張
-  {
-    "petertriho/nvim-scrollbar",
-    lazy = true,
-    event = { "BufReadPre" },
-    config = function()
-      local color_palette = require("onedark.colors")
-      require("scrollbar").setup({
-        show = true,
-        show_in_active_only = false,
-        set_highlights = true,
-        folds = 1000,
-        max_lines = false,
-        hide_if_all_visible = false,
-        throttle_ms = 100,
-        handle = {
-          color = color_palette.diff_text, -- #32526c
-        },
-        marks = {
-          Search = { color = color_palette.cyan }, -- #5fafb9
-          Error = { color = color_palette.red }, -- #e16d77
-          Warn = { color = color_palette.yellow }, -- #dfbe81
-          Info = { color = color_palette.green }, -- #99bc80
-          Hint = { color = color_palette.blue }, -- #68aee8
-          Misc = { color = color_palette.purple }, -- #c27fd7
-        },
-        handlers = {
-          cursor = true,
-          diagnostic = true,
-          gitsigns = true,
-          search = true,
-        },
-      })
-    end,
-  },
-  -- 検索結果をvirtual textで表示するプラグイン
-  {
-    "kevinhwang91/nvim-hlslens",
-    enabled = true,
-    lazy = true,
-    event = { "BufReadPost" },
-    config = function()
-      require("hlslens").setup({
-        -- 検索結果に対してvirtual text (例[▼ 1/5] )を表示する設定
-        override_lens = function(render, posList, nearest, idx, relIdx)
-          local sfw = vim.v.searchforward == 1
-          local indicator, text, chunks
-          local absRelIdx = math.abs(relIdx)
-          if absRelIdx > 1 then
-            indicator = ("%d%s"):format(absRelIdx, sfw ~= (relIdx > 1) and "▲" or "▼")
-          elseif absRelIdx == 1 then
-            indicator = sfw ~= (relIdx == 1) and "▲" or "▼"
-          else
-            indicator = ""
-          end
-
-          -- lua 5.1と5.2+の互換のため
-          local unpck = unpack or table.unpack
-          local lnum, col = unpck(posList[idx])
-          if nearest then
-            local cnt = #posList
-            if indicator ~= "" then
-              text = ("[%s %d/%s]"):format(indicator, idx, cnt)
-            else
-              text = ("[%d/%d]"):format(idx, cnt)
-            end
-            chunks = { { " " }, { text, "HLSearchLensNear" } }
-          else
-            text = ("[%s %d]"):format(indicator, idx)
-            chunks = { { " " }, { text, "HLSearchLens" } }
-          end
-          render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
-        end,
-        -- nvim-scrollbar に検索位置を表示する設定
-        build_position_cb = function(plist, _, _, _)
-          require("scrollbar.handlers.search").handler.show(plist.start_pos)
-        end,
-      })
-    end,
-  },
+  "petertriho/nvim-scrollbar",
+  lazy = true,
+  event = { "BufReadPre" },
+  config = function()
+    local color_palette = require("onedark.colors")
+    require("scrollbar").setup({
+      show = true,
+      show_in_active_only = false,
+      set_highlights = true,
+      folds = 1000,
+      max_lines = false,
+      hide_if_all_visible = false,
+      throttle_ms = 100,
+      handle = {
+        color = color_palette.diff_text, -- #32526c
+      },
+      marks = {
+        Search = { color = color_palette.cyan }, -- #5fafb9
+        Error = { color = color_palette.red }, -- #e16d77
+        Warn = { color = color_palette.yellow }, -- #dfbe81
+        Info = { color = color_palette.green }, -- #99bc80
+        Hint = { color = color_palette.blue }, -- #68aee8
+        Misc = { color = color_palette.purple }, -- #c27fd7
+      },
+      handlers = {
+        cursor = true,
+        diagnostic = true,
+        gitsigns = true,
+        search = true,
+      },
+    })
+  end,
 }
