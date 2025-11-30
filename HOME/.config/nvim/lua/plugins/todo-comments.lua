ac2af4aec20a70e8e1179d5cc23a1dda4f942386
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-11-30 19:47:34 +0900

    update: todo-comments config

--- HOME/.config/nvim/lua/plugins/todo-comments.lua
+++ HOME/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,94 +1,96 @@
 -- TODOコメントを強調表示するプラグイン
 return {
-  "folke/todo-comments.nvim",
-  lazy = true,
-  event = "VeryLazy",
-  dependencies = "nvim-lua/plenary.nvim",
-  init = function()
-    local wk = require("which-key")
-    wk.add({
-      {
-        ";tj",
-        function()
-          require("todo-comments").jump_next()
-        end,
-        mode = "n",
-        icon = "󰜮",
-        desc = "次の TODO コメントにジャンプ",
-      },
-      {
-        ";tk",
-        function()
-          require("todo-comments").jump_prev()
-        end,
-        mode = "n",
-        icon = "󰜷",
-        desc = "前の TODO コメントにジャンプ",
-      },
-    })
-  end,
-  config = function()
-    local color_palette = require("onedark.colors")
-    require("todo-comments").setup({
-      signs = true, -- show icons in the signs column
-      sign_priority = 8, -- sign priority
-      -- keywords recognized as todo comments
-      keywords = {
-        FIX = { icon = "☔", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "FAILED" } },
-        TODO = { icon = "🪧", color = "info" },
-        HACK = { icon = "🌦️", color = "warning" },
-        WARN = { icon = "⛈️", color = "warning", alt = { "WARNING", "XXX" } },
-        NOTE = { icon = "📖", color = "hint", alt = { "INFO", "HINT", "FYI" } },
-        DOCS = { icon = "📚", color = "default", alt = { "DOC" } },
-        TEST = { icon = "🧪", color = "test", alt = { "TESTING", "PASSED" } },
-        PERF = { icon = "⏳", color = "perf", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      },
-      gui_style = {
-        fg = "NONE", -- The gui style to use for the fg highlight group.
-        bg = "BOLD", -- The gui style to use for the bg highlight group.
-      },
-      merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-      -- highlighting of the line containing the todo comment
-      -- * before: highlights before the keyword (typically comment characters)
-      -- * keyword: highlights of the keyword
-      -- * after: highlights after the keyword (todo text)
-      highlight = {
-        multiline = true, -- enable multine todo comments
-        multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-        multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-        before = "", -- "fg" or "bg" or empty
-        keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-        after = "fg", -- "fg" or "bg" or empty
-        pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-        comments_only = true, -- uses treesitter to match keywords in comments only
-        max_line_len = 400, -- ignore lines longer than this
-        exclude = {}, -- list of file types to exclude highlighting
-      },
-      -- list of named colors where we try to extract the guifg from the
-      -- list of highlight groups or use the hex color if hl not found as a fallback
-      colors = {
-        error = { color_palette.red }, -- #de5d68
-        warning = { color_palette.orange }, -- #c49060
-        info = { color_palette.blue }, -- #57a5e5
-        hint = { color_palette.green }, -- #8fb573
-        default = { color_palette.dark_cyan }, -- #2b5d63
-        test = { color_palette.dark_yellow }, -- #7c5c20
-        perf = { color_palette.dark_purple }, -- #79428a
-      },
-      search = {
-        command = "rg",
-        args = {
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
+  {
+    "folke/todo-comments.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    init = function()
+      local wk = require("which-key")
+      wk.add({
+        {
+          ";tj",
+          function()
+            require("todo-comments").jump_next()
+          end,
+          mode = "n",
+          icon = "󰜮",
+          desc = "次の TODO コメントにジャンプ",
         },
-        -- regex that will be used to match keywords.
-        -- don't replace the (KEYWORDS) placeholder
-        pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-        -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-      },
-    })
-  end,
+        {
+          ";tk",
+          function()
+            require("todo-comments").jump_prev()
+          end,
+          mode = "n",
+          icon = "󰜷",
+          desc = "前の TODO コメントにジャンプ",
+        },
+      })
+    end,
+    config = function()
+      local color_palette = require("onedark.colors")
+      require("todo-comments").setup({
+        signs = true, -- show icons in the signs column
+        sign_priority = 8, -- sign priority
+        -- keywords recognized as todo comments
+        keywords = {
+          FIX = { icon = "󰻷", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "FAILED" } },
+          TODO = { icon = "󰄲", color = "info" },
+          HACK = { icon = "󰭻", color = "warning" },
+          WARN = { icon = "󰂚", color = "warning", alt = { "WARNING", "XXX" } },
+          NOTE = { icon = "󰗚", color = "hint", alt = { "INFO", "HINT", "FYI" } },
+          DOCS = { icon = "󰈙", color = "default", alt = { "DOC" } },
+          TEST = { icon = "", color = "test", alt = { "TESTING", "PASSED" } },
+          PERF = { icon = "󰔟", color = "perf", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        },
+        gui_style = {
+          fg = "NONE", -- The gui style to use for the fg highlight group.
+          bg = "BOLD", -- The gui style to use for the bg highlight group.
+        },
+        merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+        -- highlighting of the line containing the todo comment
+        -- * before: highlights before the keyword (typically comment characters)
+        -- * keyword: highlights of the keyword
+        -- * after: highlights after the keyword (todo text)
+        highlight = {
+          multiline = true, -- enable multine todo comments
+          multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+          multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+          before = "", -- "fg" or "bg" or empty
+          keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+          after = "fg", -- "fg" or "bg" or empty
+          pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+          comments_only = true, -- uses treesitter to match keywords in comments only
+          max_line_len = 400, -- ignore lines longer than this
+          exclude = {}, -- list of file types to exclude highlighting
+        },
+        -- list of named colors where we try to extract the guifg from the
+        -- list of highlight groups or use the hex color if hl not found as a fallback
+        colors = {
+          error = { color_palette.red }, -- #de5d68
+          warning = { color_palette.orange }, -- #c49060
+          info = { color_palette.blue }, -- #57a5e5
+          hint = { color_palette.green }, -- #8fb573
+          default = { color_palette.dark_cyan }, -- #2b5d63
+          test = { color_palette.dark_yellow }, -- #7c5c20
+          perf = { color_palette.dark_purple }, -- #79428a
+        },
+        search = {
+          command = "rg",
+          args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+          },
+          -- regex that will be used to match keywords.
+          -- don't replace the (KEYWORDS) placeholder
+          pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+          -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+        },
+      })
+    end,
+  },
+  { "nvim-lua/plenary.nvim" },
 }
