0fe75920a03057fa0ab146809ce0a3bd0bf9dfc8
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-11-30 19:48:23 +0900

    integrate: treesitter plugins

--- HOME/.config/nvim/lua/plugins/treesitter.lua
+++ HOME/.config/nvim/lua/plugins/treesitter.lua
@@ -96,6 +96,32 @@ return {
       })
     end,
   },
+  -- コードを分割したり結合したりできるようにするプラグイン
+  {
+    "Wansmer/treesj",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local wk = require("which-key")
+      require("treesj").setup({
+        use_default_keymaps = false,
+        check_syntax_error = true,
+        max_join_length = 120,
+        cursor_behavior = "hold",
+        notify = true,
+        dot_repeat = true,
+        on_error = nil,
+      })
+      wk.add({
+        {
+          "<leader>m",
+          "<cmd>TSJToggle<CR>",
+          mode = "n",
+          icon = "󰙅",
+          desc = "配列やオブジェクトを分割/結合する",
+        },
+      })
+    end,
+  },
   -- 対応するキーワードや記号にジャンプできる
   {
     "andymass/vim-matchup",
