84ae72138dd350442215bc3713f72229cb6635e6
sugawarayss <sugawarayss@inap-vision.co.jp>
2025-12-02 16:10:28 +0900

    update: relative absolute line numbers plugin

--- HOME/.config/nvim/lua/plugins/numbers.lua
+++ HOME/.config/nvim/lua/plugins/numbers.lua
@@ -1,16 +1,20 @@
 -- Insert Mode時に絶対行表示にする
+-- return {
+--   "myusuf3/numbers.vim",
+--   -- 遅延読み込みする
+--   lazy = true,
+--   -- Insert Mode時に絶対行表示にする
+--   event = { "InsertEnter" },
+--   config = function()
+--     vim.g.numbers_exclude = {
+--       -- snacks.nvim のピッカーには表示しない
+--       "snacks_picker_list",
+--       "snacks_terminal",
+--       "snacks_dashboard",
+--     }
+--   end,
+-- }
 return {
-  "myusuf3/numbers.vim",
-  -- 遅延読み込みする
-  lazy = true,
-  -- Insert Mode時に絶対行表示にする
-  event = { "InsertEnter" },
-  config = function()
-    vim.g.numbers_exclude = {
-      -- snacks.nvim のピッカーには表示しない
-      "snacks_picker_list",
-      "snacks_terminal",
-      "snacks_dashboard",
-    }
-  end,
+  "sitiom/nvim-numbertoggle",
+  event = { "BufReadPost", "BufNewFile" },
 }
