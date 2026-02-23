b34f34b02463b10f10c5940c2c7df2aee797d055
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-02-23 11:56:33 +0900

    update: snacks.nvim chunk enable

--- HOME/.config/nvim/lua/plugins/snacks.lua
+++ HOME/.config/nvim/lua/plugins/snacks.lua
@@ -374,7 +374,7 @@ return {
     chunk = {
       -- when enabled, scopes will be rendered as chunks, except for the
       -- top-level scope which will be rendered as a scope.
-      enabled = false,
+      enabled = true,
       -- only show chunk scopes in the current window
       only_current = false,
       priority = 200,
