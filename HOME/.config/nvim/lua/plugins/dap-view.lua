39e78b73a497a3ee21018376768c6fdf809447d6
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-02-24 19:12:13 +0900

    update: nvim-dap-view config

--- HOME/.config/nvim/lua/plugins/dap-view.lua
+++ HOME/.config/nvim/lua/plugins/dap-view.lua
@@ -33,15 +33,57 @@ return {
         position = "right",
       },
     },
+    -- windows = {
+    --   size = 0.25,
+    --   position = "below",
+    --   terminal = {
+    --     size = 0.5,
+    --     position = "left",
+    --     hide = {},
+    --   },
+    -- },
     windows = {
-      size = 0.25,
-      position = "below",
+      anchor = function()
+        -- Anchor to the first terminal window found in the current tab
+        -- Tweak according to your needs
+        local windows = vim.api.nvim_tabpage_list_wins(0)
+
+        for _, win in ipairs(windows) do
+          local bufnr = vim.api.nvim_win_get_buf(win)
+          if vim.bo[bufnr].buftype == "snacks_terminal" then
+            return win
+          end
+        end
+      end,
+      position = function(prev)
+        local wins = vim.api.nvim_tabpage_list_wins(0)
+        if vim.iter(wins):find(function(win)
+          return vim.w[win].dapview_win_term
+        end) then
+          return prev
+        end
+
+        return vim.tbl_count(vim
+          .iter(wins)
+          :filter(function(win)
+            local buf = vim.api.nvim_win_get_buf(win)
+            local valid_buftype = vim.tbl_contains({ "", "help", "prompt", "quickfix", "terminal" }, vim.bo[buf].buftype)
+            local dapview_win = vim.w[win].dapview_win or vim.w[win].dapview_win_term
+            return valid_buftype and not dapview_win
+          end)
+          :totable()) > 1 and "below" or "right"
+      end,
+      size = function(pos)
+        return pos == "below" and 0.25 or 0.5
+      end,
       terminal = {
+        position = function(pos)
+          return pos == "below" and "right" or "below"
+        end,
         size = 0.5,
-        position = "left",
-        hide = {},
       },
     },
+
     -- Auto open when a session is started and auto close when all sessions finish
     -- Alternatively, can be a string:
     -- - "keep_terminal": as above, but keeps the terminal when the session finishes
