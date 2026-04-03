5d4f1c77db77589098ae8406e9ae594deadd48a9
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-04-03 17:01:14 +0900

    remove: kensaku-command.vim

--- HOME/.config/nvim/lua/plugins/kensaku.lua
+++ HOME/.config/nvim/lua/plugins/kensaku.lua
@@ -8,11 +8,11 @@ return {
   },
   { "vim-denops/denops.vim", lazy = false },
   -- 日本語をローマ字で検索できるKensakuコマンドを使えるようにする
-  {
-    "lambdalisue/kensaku-command.vim",
-    enabled = true,
-    lazy = false,
-  },
+  -- {
+  --   "lambdalisue/kensaku-command.vim",
+  --   enabled = true,
+  --   lazy = false,
+  -- },
   -- /コマンドでローマ字で日本語を検索できるようにする
   {
     "lambdalisue/kensaku-search.vim",
