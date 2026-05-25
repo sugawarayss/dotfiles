01d034d84a2756bce5718f493403c557bae8819f
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-05-25 13:12:35 +0900

    update: markview.nvim config

--- HOME/.config/nvim/lua/plugins/markdown.lua
+++ HOME/.config/nvim/lua/plugins/markdown.lua
@@ -4,9 +4,22 @@ return {
     "OXY2DEV/markview.nvim",
     ft = { "markdown", "obsidian_markdown", "codecompanion" },
     cond = function()
+      vim.g.markview_blink_loaded = true
       return not vim.g.vscode
     end,
     opts = {
+      experimental = {
+        -- テキストファイルをNeovim内で開く(file_open_commandの設定も必要)
+        prefer_nvim = true,
+        file_open_command = "tabnew",
+        date_formats = {
+          "^%d%d%d%d%-%d%d%-%d%d$",
+        },
+        date_time_formats = {
+          "^%a%a%a %a%a%a %d%d %d%d%:%d%d%:%d%d ... %d%d%d%d$", --- UNIX date time
+          "^%d%d%d%d%-%d%d%-%d%dT%d%d%:%d%d%:%d%dZ$", --- ISO 8601
+        },
+      },
       preview = {
         filetypes = { "markdown", "obsidian_markdown", "codecompanion" },
         ignore_buftypes = { "blink-cmp-documentation" },
