04bfb947130d512290c5e13319363ad391bce4fb
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-07-17 16:54:06 +0900

    update: neoscroll.nvim config

--- HOME/.config/nvim/lua/plugins/neoscroll.lua
+++ HOME/.config/nvim/lua/plugins/neoscroll.lua
@@ -17,12 +17,12 @@ return {
         "zz", -- カーソル位置が画面の中央になるようにスクロール
         "zb", -- カーソル位置が画面の一番下になるようにスクロール
       },
-      hide_cursor = true, -- Hide cursor while scrolling
-      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      hide_cursor = true, -- スクロール中はカーソルを非表示にする
+      stop_eof = true, -- スクロールをファイルの末尾で止める
       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-      duration_multiplier = 1.0, -- Global duration multiplier
-      easing = "circular", -- Default easing function. linear | quadratic | cubic | quartic | quintic | circular | sine
+      duration_multiplier = 0.3, -- Global duration multiplier
+      easing = "sine", -- Default easing function. linear | quadratic | cubic | quartic | quintic | circular | sine
       pre_hook = nil, -- Function to run before the scrolling animation starts
       post_hook = nil, -- Function to run after the scrolling animation ends
       performance_mode = false, -- Disable "Performance Mode" on all buffers.
