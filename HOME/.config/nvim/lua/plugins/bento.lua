e36feed084fb14ed6d3b804276639fcec459a9d0
sugawarayss <sugawarayss@inap-vision.co.jp>
2026-09-08 14:39:04 +0900

    update: bento.nvim main key

--- HOME/.config/nvim/lua/plugins/bento.lua
+++ HOME/.config/nvim/lua/plugins/bento.lua
@@ -7,7 +7,7 @@ return {
   end,
   config = function()
     require("bento").setup({
-      main_keymap = ";", -- メニューのトグル(展開)キー
+      main_keymap = ",", -- メニューのトグル(展開)キー
       lock_char = "🔒", -- ロックされたバッファ名の前に表示される文字
       max_open_buffers = 5, -- 開いたままにしておくバッファの最大数(nilは無制限)
       --   recency_access: 最近アクセス(entered/viewed)されていないバッファを削除
